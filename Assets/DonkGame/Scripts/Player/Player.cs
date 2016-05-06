197611c331554cadbef84d99cf6a9601bed81ea8
Adam Proctor <adam@uken.com>
2016-05-06 16:32:07 -0400

    some chicken egg stuff

--- Assets/DonkGame/Scripts/Player/Player.cs
+++ Assets/DonkGame/Scripts/Player/Player.cs
@@ -80,46 +80,71 @@ public class Player : MonoBehaviour {
         }
     }
 
-    void UpdateActions() {
-        //The following comments assume xbox 360 controls, but it should work cross platform
+  void UpdateActions ()
+  {
+    //The following comments assume xbox 360 controls, but it should work cross platform
 
-        if (device.Action1.WasPressed) {
-          //A button          
-        }
-        if (device.Action2.WasPressed) {
-            //B button
-            Debug.LogError("Player " + playerNumber + " B");
-        }
-        if (device.Action3.WasPressed || device.RightTrigger.WasPressed) {
-            //X button
-            this.Attack();
-        }
-        if (device.Action4.WasPressed) {
-            //Y Button
-            Debug.LogError("Player " + playerNumber + " Y");
-        }  
+    if (device.Action1.WasPressed) {
+      //A button          
+      PrimaryAction();
     }
+    if (device.Action2.WasPressed) {
+      //B button
+      Debug.LogError ("Player " + playerNumber + " B");
+    }
+    if (device.Action3.WasPressed || device.RightTrigger.WasPressed) {
+      //X button
+      this.Attack ();
+    }
+    if (device.Action4.WasPressed) {
+      //Y Button
+      Debug.LogError ("Player " + playerNumber + " Y");
+    }  
+  }
 
 
-	private void AddAbility(GameObject abilityPrefab) {
-		GameObject abilityInstance = (GameObject)GameObject.Instantiate (abilityPrefab);
+    private void AddAbility(GameObject abilityPrefab) {
+    	GameObject abilityInstance = (GameObject)GameObject.Instantiate (abilityPrefab);
 
-		Ability a = abilityInstance.GetComponent<Ability>();
-		a.transform.SetParent (this.abilityContainer);
-		a.Init(this.transform, this.attackMask, this);
+    	Ability a = abilityInstance.GetComponent<Ability>();
+    	a.transform.SetParent (this.abilityContainer);
+    	a.Init(this.transform, this.attackMask, this);
 
-		//TODO multi slot
-		this.Abilities[0] = a;
-	}
-
+    	//TODO multi slot
+    	this.Abilities[0] = a;
+    }
+    
     void Attack() {		
       if(this.Abilities[0] != null) {
         this.Abilities[0].Activate();
       }
     }
 
+  public float tempActivateRadius = 3f;
+  public Vector3 activateBoxCenter = new Vector3 (0f, 0f, 1.5f);
+  public Vector3 activateBoxSize = Vector3.one * 3;
+  void PrimaryAction () {
+    //TODO make this properly rotate
+    Collider[] possibleObjects = Physics.OverlapBox(this.transform.position, activateBoxSize, this.transform.rotation);
+
+    for (int i = 0; i < possibleObjects.Length; i++) {
+      BigEgg egg = possibleObjects [i].GetComponent<BigEgg> ();
+      if (egg != null) {
+        Debug.LogError ("Latching onto egg");
+        break;
+      }
+
+      Chicken chicken = possibleObjects [i].GetComponent<Chicken> ();
+      if (chicken != null) {
+        Debug.LogError ("eating chicken");
+        break;
+      }
+    }
+  }
+
     void OnDrawGizmosSelected() {
         Gizmos.color = Color.magenta;
         Gizmos.DrawSphere(this.transform.position + this.aimDirection, 0.3f);
+    Gizmos.DrawWireCube (this.transform.position + this.activateBoxCenter, activateBoxSize);
     }
 }
