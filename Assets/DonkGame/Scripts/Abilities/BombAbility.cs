249259ff4a78c367b04e1e54c3ccf7bac82a71dd
Adam Proctor <adam@uken.com>
2016-05-07 14:50:41 -0400

    bomb shop

---
+++ Assets/DonkGame/Scripts/Abilities/BombAbility.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class BombAbility : Ability {
+
+	public int damageAmount = 30;
+	[SerializeField]
+  	private float radius = 2;
+
+	public GameObject projectilePrefab;
+
+    public override void Activate() {
+		GameObject spawnedProjectile = (GameObject)GameObject.Instantiate (projectilePrefab);
+		spawnedProjectile.transform.position = this.transform.position + this.transform.forward * 2;
+    }
+}
