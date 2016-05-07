44df6e0ab9ae8258248fd827c654dd0f720b85ce
Yeruot <Keelan1013@gmail.com>
2016-05-07 18:33:59 -0400

    chicken spitting update

--- Assets/DonkGame/Scripts/Pickups/Chicken.cs
+++ Assets/DonkGame/Scripts/Pickups/Chicken.cs
@@ -5,25 +5,37 @@ public class Chicken : MonoBehaviour {
   public float weight = 10;
 
   private Rigidbody myRigidbody;
+  private bool standUp;
 
   void Start() {
     this.myRigidbody = this.GetComponent<Rigidbody>();
   }
+
+  void Update() {
+//    if (this.standUp) {
+//      this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.Euler(0f, this.transform.rotation.y, 0f), 0.2f);
+//      this.standUp = !((Mathf.Approximately(0.2f, this.transform.rotation.x) && Mathf.Approximately(0.2f, this.transform.rotation.z)));
+//    }
+  }
+
   public IEnumerator SpatOut() {
 		Collider c = this.GetComponent<Collider>();
 
     this.gameObject.SetActive(true);
-    this.transform.parent = null;
-    this.myRigidbody.isKinematic = false;
     c.enabled = false;
-    this.myRigidbody.AddForce(Vector3.up * 500f);
+    this.myRigidbody.AddForce((this.transform.parent.up *Random.Range(400f, 500f)) + (-this.transform.parent.forward * Random.Range(100f, 150f)) + (this.transform.parent.right * Random.Range(-200f, 200f)));
+    this.transform.parent = null;
 
     yield return new WaitForSeconds(1);
     c.enabled = true;
-    yield return new WaitForSeconds(5);
-    this.myRigidbody.isKinematic = true;
 
     yield return null;
+  }
+
+  void OnCollisionEnter(Collision collision) {
+    if(collision.gameObject.name == "Ground") {
+      this.standUp = true;
+    }
   }
 
 }
