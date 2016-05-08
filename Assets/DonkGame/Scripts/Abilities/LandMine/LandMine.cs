8171e1db74ed08ed010fa7d02d8b3134f3cb8ef9
Yeruot <Keelan1013@gmail.com>
2016-05-07 21:02:03 -0400

    land mine

---
+++ Assets/DonkGame/Scripts/Abilities/LandMine/LandMine.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LandMine : MonoBehaviour {
+
+  [SerializeField]
+  float damage;
+  [SerializeField]
+  GameObject explosionSystem;
+
+  void OnTriggerEnter(Collider collider) {
+    Damagable damagable = collider.gameObject.GetComponent<Damagable>();
+    if(damagable != null) {
+      damagable.TakeDamage(this.damage);
+    }
+
+    this.explosionSystem.SetActive(true);
+
+    Destroy(this.gameObject, 2f);
+
+  }
+}
